c7ba4f76f7edb54d75a4e8a914c643e830eea5e4
Chi Tran <chidtran@gmail.com>
2019-02-25 20:40:08 -0700

    Moves split_message_into_groups_of_four_chars method to Shifts mod

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,13 +2,5 @@ require './lib/shifts'
 
 class Enigma
   include Shifts 
-
-  def split_message_into_groups_of_four_chars(message)
-    grouped_by_four = []
-    message.chars.each_slice(4) do |four_chars|
-      grouped_by_four << four_chars
-    end
-    grouped_by_four
-  end
   
 end 
--- lib/shifts.rb
+++ lib/shifts.rb
@@ -38,4 +38,12 @@ module Shifts
       shift_char(character, shift_values[index])
     end 
   end
+
+  def split_message_into_groups_of_four_chars(message)
+    grouped_by_four = []
+    message.chars.each_slice(4) do |four_chars|
+      grouped_by_four << four_chars
+    end
+    grouped_by_four
+  end
 end 
