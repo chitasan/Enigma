bc2aedc6755e1d238df605fbe6d63630b139913c
Chi Tran <chidtran@gmail.com>
2019-02-25 19:46:59 -0700

    Adds encrypt to testhelper, adds .new to tests to have it pass

--- test/encrypt_test.rb
+++ test/encrypt_test.rb
@@ -3,25 +3,25 @@ require 'pry'
 
 class EncryptTest < Minitest::Test
   def test_it_exists 
-    encrypt = Encrypt("hello world", "02715", "040895")
+    encrypt = Encrypt.new("hello world", "02715", "040895")
 
     assert_instance_of Encrypt, encrypt
   end 
 
   def test_it_has_message
-    encrypt = Encrypt("hello world", "02715", "040895")
+    encrypt = Encrypt.new("hello world", "02715", "040895")
 
     assert_equal "hello world", encrypt.message
   end 
 
   def test_it_has_key
-    encrypt = Encrypt("hello world", "02715", "040895")
+    encrypt = Encrypt.new("hello world", "02715", "040895")
 
     assert_equal "02715", encrypt.key
   end
 
   def test_it_has_date
-    encrypt = Encrypt("hello world", "02715", "040895")
+    encrypt = Encrypt.new("hello world", "02715", "040895")
 
     assert_equal "040895", encrypt.date
   end 
