24866ce4ee7dcd6f2018628990e2ea1ed311e677
Chi Tran <chidtran@gmail.com>
2019-02-25 18:05:35 -0700

    Setups skeleton files

---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'minitest/autorun'
+require 'minitest/pride'
+
+require './lib/enigma'
+require './lib/date_offset'
+require './lib/key_offset'
+require './lib/shifts'
+require './lib/generator'
