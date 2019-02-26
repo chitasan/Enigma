37cc1e39b55d1e3887f84ce5b076fc87200338bd
Chi Tran <chidtran@gmail.com>
2019-02-25 22:18:05 -0700

    Adds test to test uppercased letters in Enigma

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,4 +17,16 @@ class EnigmaTest < Minitest::Test
     }
     assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end 
+
+  def test_it_can_encrypt_a_message_with_uppercase_letters
+    enigma = Enigma.new
+
+    expected = {
+      encryption: "keder ohulw!",
+      key: "02715",
+      date: "040895"
+    }
+    assert_equal expected, enigma.encrypt("Hello World!", "02715", "040895")
+    assert_equal expected, enigma.encrypt("HellO WorlD!", "02715", "040895")
+  end
 end 
