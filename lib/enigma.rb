76bb145654166d0dc093c19921a989b2f990c1ef
Chi Tran <chidtran@gmail.com>
2019-02-25 18:15:13 -0700

    Adds generate_key method to Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,11 @@
 class Enigma
   def character_set #to Generator
     character_set = ("a".."z").to_a << " " 
+  end
+  
+  def generate_key #to Generator
+    (0..9).to_a.sample(5).join("")
   end 
+
+  
 end 
