b2289e63a2abe158014c284590243751d2048133
Chi Tran <chidtran@gmail.com>
2019-02-25 21:31:14 -0700

    Moves split_message_into_groups_of_four_chars into Cipher class from Shifts mod

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -10,4 +10,12 @@ class Cipher
     @key = key
     @date = date 
   end
+
+  def split_message_into_groups_of_four_chars(message)
+    grouped_by_four = []
+    message.chars.each_slice(4) do |four_chars|
+      grouped_by_four << four_chars
+    end
+    grouped_by_four
+  end
 end 
