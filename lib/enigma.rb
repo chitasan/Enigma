65621bc42f22cc4b2051060a74959f7a761b6a5b
Chi Tran <chidtran@gmail.com>
2019-02-25 18:47:09 -0700

    Moves create_shifts, shift_char, shift_four_chars to Shifts mode from Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,17 +1,7 @@
-require './lib/key_offset'
-require './lib/date_offset'
+require './lib/shifts'
 
 class Enigma
-  include KeyOffset
-  include DateOffset  
-
-  def create_shifts(key, date)
-    date = date_offsets(date)
-    key = paired_number_offsets(key)
-    date.merge(key) do |letter, date, key|
-      date + key
-    end 
-  end
+  include Shifts 
 
   def split_message_into_groups_of_four_chars(message)
     grouped_by_four = []
@@ -21,19 +11,4 @@ class Enigma
     grouped_by_four
   end
   
-  def shift_char(character, shift_value)
-    if character_set.include?(character)
-      index = character_set.find_index(character)
-      shifted_char = character_set.rotate(shift_value)
-      shifted_char[index]
-    else
-      character
-    end 
-  end
-
-  def shift_four_chars(characters, shift_values)
-    characters.map.with_index do |character, index|
-      shift_char(character, shift_values[index])
-    end 
-  end
 end 
---
+++ lib/shifts.rb
@@ -0,0 +1,31 @@
+require './lib/key_offset'
+require './lib/date_offset'
+
+module Shifts   
+  include KeyOffset
+  include DateOffset
+
+  def create_shifts(key, date)
+    date = date_offsets(date)
+    key = paired_number_offsets(key)
+    date.merge(key) do |letter, date, key|
+      date + key
+    end 
+  end 
+
+  def shift_char(character, shift_value)
+    if character_set.include?(character)
+      index = character_set.find_index(character)
+      shifted_char = character_set.rotate(shift_value)
+      shifted_char[index]
+    else
+      character
+    end 
+  end
+
+  def shift_four_chars(characters, shift_values)
+    characters.map.with_index do |character, index|
+      shift_char(character, shift_values[index])
+    end 
+  end
+end 
