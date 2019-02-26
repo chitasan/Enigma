167cb266f7b3b260b316464b888fe24b541dc3c4
Chi Tran <chidtran@gmail.com>
2019-02-25 22:34:18 -0700

    Adds decrypt method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,7 +3,20 @@ require './lib/cipher'
 class Enigma
   def encrypt(message, key, date)
     cipher = Cipher.encrypt(message, key, date)
-    {encryption: cipher, key: key, date: date}
-  end 
+    {
+      encryption: cipher, 
+      key: key, 
+      date: date
+    }
+  end
+
+   def decrypt(message, key, date)
+    cipher = Cipher.decrypt(message, key, date)
+    {
+      decryption: cipher, 
+      key: key, 
+      date: date
+    }
+  end
   
 end 
