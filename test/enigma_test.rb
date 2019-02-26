2202586cb06a6c4ce7061fe8d2bbc575f7b5d69a
Chi Tran <chidtran@gmail.com>
2019-02-25 18:12:09 -0700

    Adds test for character_set

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,4 +6,14 @@ class EnigmaTest < Minitest::Test
     enigma = Enigma.new
     assert_instance_of Enigma, enigma
   end 
+
+  def test_it_has_a_character_set_of_27
+    enigma = Enigma.new
+   
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, enigma.character_set
+    assert_equal 27, enigma.character_set.count
+  end
+
+  
 end 
