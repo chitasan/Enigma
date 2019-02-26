44ec1464c0fbd821272b6ba07d0eefa1712aa1b5
Chi Tran <chidtran@gmail.com>
2019-02-25 21:37:15 -0700

    Adds tests for Cipher class

--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -22,19 +22,33 @@ class CipherTest < Minitest::Test
 
   def test_it_has_date
     cipher = Cipher.new("hello world", "02715", "040895")
-
-    assert_equal "040895", cipher.date
   end 
 
-  def test_it_can_cipher_a_message_with_a_key_and_a_date
-    skip
-    cipher = Cipher.new("hello world", "02715", "040895")
+  def test_it_can_encrypt_message_from_self
+    cipher = Cipher.encrypt("hello world!", "02715", "040895")
 
-    expected = {
-      cipherion: "keder ohulw",
-      key: "02715",
-      date: "040895"
-   }
-    assert_equal expected, cipher.execute 
+    assert_equal 'keder ohulw!', cipher
+  end
+
+  def test_it_can_encrypt_a_message
+    cipher = Cipher.new("hello world!", "02715", "040895")
+    assert_equal 'keder ohulw!', cipher.encrypt
+  end 
+
+  def test_it_can_decrypt_a_message
+    cipher = Cipher.new("keder ohulw!", "02715", "040895")
+    assert_equal 'hello world!', cipher.decrypt
+  end
+
+  def test_it_can_return_shifts_values
+   cipher = Cipher.new("hello world!", "02715", "040895")
+
+    assert_equal [3, 27, 73, 20], cipher.shifts
+  end 
+
+  def test_it_can_return_negative_shifts
+    cipher = Cipher.new("hello world!", "02715", "040895")
+
+    assert_equal [-3, -27, -73, -20], cipher.negative_shifts
   end 
 end 
