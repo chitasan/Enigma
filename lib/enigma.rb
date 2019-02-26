43e4468f1f19eb0a7df82d113c75e901d5ca9d3d
Chi Tran <chidtran@gmail.com>
2019-02-25 18:24:40 -0700

    Adds date_offsets method to Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -48,4 +48,13 @@ class Enigma
     date_squared = date.to_i ** 2
     date_squared.digits.reverse.last(4)
   end
+
+  def date_offsets(date)
+    last_four = squared_date_last_four(date)
+    date_offsets_hash = {}
+    offsets_hash_keys.each_with_index do |letter, index|
+      date_offsets_hash[letter] = last_four[index].to_i
+    end
+    date_offsets_hash
+  end  
 end 
